e624559b8238fd640c331983f54766d20f4d7fc7
Qadoos <mgulraiz00@gmail.com>
2023-10-29 11:43:49 +0500

    it is school register

---
+++ schoolRegister.ts
@@ -0,0 +1,21 @@
+
+class register{
+    name:string;
+    page:number;
+    items: number;
+    incharge:string
+    constructor(name:string,page:number,items:number,incharge:string){
+        this.name=name,
+        this.page=page,
+        this.items=items,
+        this.incharge=incharge
+    }
+    schoolregister(){
+        return 'register name is ${this.name} and pages are ${this.page}'
+    }
+}
+const register1 =new register("admission",100,90, "naveed")
+const register2 =new register("science lab",100,90, "yasir")
+const register3 =new register("compute",100,90, "Ahtesham")
+const register4 =new register("physics", 200, 25, "haider sb")
+console.log(register1,register2,register3,register4)
