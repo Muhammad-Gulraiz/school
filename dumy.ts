dc472ae62f1217ea058c4412827f4f0af64d9cc5
Qadoos <mgulraiz00@gmail.com>
2023-10-29 15:08:15 +0500

    ternery file

--- dumy.ts
+++ dumy.ts
@@ -614,28 +614,7 @@
 
 // ===================================================
 // 30
-// Ternary operator:
-// Condition or conditions ? code 1 : code 2
 
-// let username = "Aashir"
-// if(username== "Aashir"){
-//     console.log("you are so sweet")
-// }else{
-//     console.log("It is invalid usearname")
-// }
-// Ternary operator:
-// Condition or conditions ? code 1 : code 2
-
-// let username = "Aashir"
-// username== "Aashir" ? 
-// console.log("you are so sweet") : 
-// console.log("It is invalid usearname")
-
-// let username = "Aashir"
-// let password = 133
-// username== "Aashir" && password == 123 ? 
-// console.log("you are so sweet") : 
-// console.log("It is invalid usearname")
 // ================================================
 // 25
 
@@ -656,13 +635,11 @@
 
 // 
 // ========================================================================
-// let pow = Math.pow(5,3)
-// console.log(pow) 
+ 
 
 
 // =======================================================================================
-// let Pi =Math.PI
-//  console.log(Pi)
+
 // ======================================================================
 
 //     class calculator{
---
+++ ternery.ts
@@ -0,0 +1,22 @@
+// Ternary operator:
+// Condition or conditions ? code 1 : code 2
+
+// let username = "Aashir"
+// if(username== "Aashir"){console.log("you are so sweet")
+// }else{
+//     console.log("It is invalid usearname")
+// }
+
+// Ternary operator:
+// Condition or conditions ? code 1 : code 2
+
+let username = "Aashir"
+username== "Aashir" ? 
+console.log("you are so sweet") : 
+console.log("It is invalid usearname")
+
+// let username = "Aashir"
+// let password = 133
+// username== "Aashir" && password == 123 ? 
+// console.log("you are so sweet") : 
+// console.log("It is invalid usearname")
