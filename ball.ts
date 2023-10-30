388b579addd9a57d10b26ebf2b9a4c19e7a98b71
Qadoos <mgulraiz00@gmail.com>
2023-10-30 23:25:32 +0500

    hello pakistan

---
+++ ball.ts
@@ -0,0 +1,16 @@
+let ali = {
+    firstname:"sweet",
+   lastname:"heart",
+   age: 12,
+   hobbies:["aa", "bb", "CC"]
+   salary:()=>  "month",
+   email:"mgulraiz00@gmail.com"
+   
+}
+delete ali.salary 
+ali.adress = "house#306.NR masjid mochian jada jhelum",
+console.log(ali)
+
+
+let x:number = Math.round(10.3);
+console.log(x);
