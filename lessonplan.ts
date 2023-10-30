abb0b383587f8cc1f242315a32727871d02bd094
Qadoos <mgulraiz00@gmail.com>
2023-10-31 00:18:58 +0500

    this is rose

--- lessonplan.ts
+++ lessonplan.ts
@@ -1,38 +1,38 @@
-// class lessonPlan{
-//     name:string;
-//     subject:string;
-//     chapter:number;
-//     topic:string
-//     monday:string;
-//     tuesdeay:string;
-//     wednesday:string;
-//     thursday:string;
-//     friday:string
-//     constructor(name:string,subject:string,chapter:number,topic:string,monday:string,tuesday:string,wednesday:string,thursday:string,friday:string){
-//         this.name=name
-//         this.subject=subject
-//         this.chapter=chapter
-//         this.topic=topic
-//         this.monday=monday
-//         this.tuesdeay=tuesday
-//         this.wednesday=wednesday
-//         this.thursday=thursday
-//         this.friday=friday
-//     }
-//     oddRollnumber(){
-//         return 'lesson name is ${this.name}'
-//     }
-//     evenRollnumber(){
-//         return 'subject is ${this.subject} and chapter is ${this.chapter} and topic is ${this.topic}'
-//     }
-//     lessonDays(){
-//         return 'on monday is ${this.monday} and on tuesday is ${this.tuesday} and on wednesday is ${this.wednesday} and on thursday is ${this.thursday} and on friday is ${this.friday}'
-//     }
-// }
-// const lessonPlan1 = new lessonPlan("SirHaroon","Physics",12,"force of gravity","lecture","practice","test","result","practicle")
-// const lessonPlan2 = new lessonPlan("SirNaseem","Biology",10,"circulatory system","lecture","practice","test","result","practicle")
-// const lessonPlan3 = new lessonPlan("SirAqib","Chemistery",2,"scheilding effect","lecture","practice","test","result","practicle")
-// const lessonPlan4 = new lessonPlan("SirTariq","Maths",5,"partial fractions","lecture","practice","test","result","practicle")
+class lessonPlan{
+    name:string;
+    subject:string;
+    chapter:number;
+    topic:string
+    monday:string;
+    tuesdeay:string;
+    wednesday:string;
+    thursday:string;
+    friday:string
+    constructor(name:string,subject:string,chapter:number,topic:string,monday:string,tuesday:string,wednesday:string,thursday:string,friday:string){
+        this.name=name
+        this.subject=subject
+        this.chapter=chapter
+        this.topic=topic
+        this.monday=monday
+        this.tuesdeay=tuesday
+        this.wednesday=wednesday
+        this.thursday=thursday
+        this.friday=friday
+    }
+    oddRollnumber(){
+        return 'lesson name is ${this.name}'
+    }
+    evenRollnumber(){
+        return 'subject is ${this.subject} and chapter is ${this.chapter} and topic is ${this.topic}'
+    }
+    lessonDays(){
+        return 'on monday is ${this.monday} and on tuesday is ${this.tuesday} and on wednesday is ${this.wednesday} and on thursday is ${this.thursday} and on friday is ${this.friday}'
+    }
+}
+const lessonPlan1 = new lessonPlan("SirHaroon","Physics",12,"force of gravity","lecture","practice","test","result","practicle")
+const lessonPlan2 = new lessonPlan("SirNaseem","Biology",10,"circulatory system","lecture","practice","test","result","practicle")
+const lessonPlan3 = new lessonPlan("SirAqib","Chemistery",2,"scheilding effect","lecture","practice","test","result","practicle")
+const lessonPlan4 = new lessonPlan("SirTariq","Maths",5,"partial fractions","lecture","practice","test","result","practicle")
 
-// let x = lessonPlan1, y = lessonPlan2, z = lessonPlan3, s = lessonPlan4
-// console.log(x,y,z,s)
+let h = lessonPlan1, e = lessonPlan2, f = lessonPlan3, g = lessonPlan4
+console.log(h,e,f,g)
