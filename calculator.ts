fb69025dc20f4ae6fe69d3046a9dd0caef3c18b6
Qadoos <mgulraiz00@gmail.com>
2023-11-04 23:07:31 +0500

    please check the calculator

--- calculator.ts
+++ calculator.ts
@@ -1,40 +1,104 @@
-class Calculator{       
+// class Calculator{       
+//     answer: number = 0;    
+      
+//     add(n1:number,n2:number):number{
+//         return this.answer = n1 +n2
+//     }
+//     sub(n1:number,n2:number):number{
+//         return this.answer = n1 - n2
+//     }
+//     mul(n1:number,n2:number):number{
+//         return this.answer = n1 * n2
+//     }
+//     div(n1:number,n2:number):number{
+//         return this.answer = n1 / n2        
+//     }    
+//     pow(n1:number,n2:number):number{
+//         return this.answer = Math.pow(n1,n2)
+//     }
+//     log10(n:number):number{
+//         return this.answer= Math.log10(n)
+//     }
+//     sqrt(n:number):number{
+//         return this.answer = Math.sqrt(n)
+//     }
+//     cos(n:number):number{
+//         return this.answer = Math.cos(n)
+//     }
+//     acos(n:number):number{
+//         return this.answer = Math.acos(n)
+//     }
+//     sin(n:number):number{
+//         return this.answer = Math.sin(n)
+//     }
+//     tan(n:number):number{
+//         return this.answer = Math.tan(n)
+//     }
+// }
+// const w = new Calculator();
+// w.add(5,2)
+// console.log(w)
+
+
+// console.log("start")
+
+// try {
+//     let x = "Ashir"
+//     console.log(x)
+// } catch (error) {
+//     console.log("error")
+// }
+
+// console.log("end")
+
+
+console.log("start")
+
+try {
+    // let x = "Ashir"
+    // console.log(x)
+
+    class Calculator{       
     answer: number = 0;    
       
     add(n1:number,n2:number):number{
         return this.answer = n1 +n2
     }
     sub(n1:number,n2:number):number{
-        return this.answer = n1 - n2
+                return this.answer = n1 - n2
+            }
+            mul(n1:number,n2:number):number{
+                return this.answer = n1 * n2
+            }
+            div(n1:number,n2:number):number{
+                return this.answer = n1 / n2        
+            }    
+            pow(n1:number,n2:number):number{
+                return this.answer = Math.pow(n1,n2)
+            }
+            log10(n:number):number{
+                return this.answer= Math.log10(n)
+            }
+            sqrt(n:number):number{
+                return this.answer = Math.sqrt(n)
+            }
+            cos(n:number):number{
+                return this.answer = Math.cos(n)
+            }
+            acos(n:number):number{
+                return this.answer = Math.acos(n)
+            }
+            sin(n:number):number{
+                return this.answer = Math.sin(n)
+            }
+            tan(n:number):number{
+                return this.answer = Math.tan(n)
+            }
     }
-    mul(n1:number,n2:number):number{
-        return this.answer = n1 * n2
-    }
-    div(n1:number,n2:number):number{
-        return this.answer = n1 / n2
-    }
-    pow(n1:number,n2:number):number{
-        return this.answer = Math.pow(n1,n2)
-    }
-    log10(n:number):number{
-        return this.answer= Math.log10(n)
-    }
-    sqrt(n:number):number{
-        return this.answer = Math.sqrt(n)
-    }
-    cos(n:number):number{
-        return this.answer = Math.cos(n)
-    }
-    acos(n:number):number{
-        return this.answer = Math.acos(n)
-    }
-    sin(n:number):number{
-        return this.answer = Math.sin(n)
-    }
-    tan(n:number):number{
-        return this.answer = Math.tan(n)
-    }
+    const w = new Calculator();
+w.sin(30,9)
+console.log(w)
+} catch (error) {
+    console.log("error")
 }
-const calculator1 = new Calculator();
-calculator1.sin(30)
-console.log(calculator1)
+console.log("end")
