1d32e5fb543126d502394221e365b39d1524b15b
Qadoos <mgulraiz00@gmail.com>
2023-11-10 21:04:42 +0500

    setting the format of calculator

--- calculator.ts
+++ calculator.ts
@@ -63,35 +63,35 @@ console.log("start")
         return this.answer = n1 +n2
     }
     sub(n1:number,n2:number):number{
-                return this.answer = n1 - n2
-            }
-            mul(n1:number,n2:number):number{
-                return this.answer = n1 * n2
-            }
-            div(n1:number,n2:number):number{
-                return this.answer = n1 / n2        
-            }    
-            pow(n1:number,n2:number):number{
-                return this.answer = Math.pow(n1,n2)
-            }
-            log10(n:number):number{
-                return this.answer= Math.log10(n)
-            }
-            sqrt(n:number):number{
-                return this.answer = Math.sqrt(n)
-            }
-            cos(n:number):number{
-                return this.answer = Math.cos(n)
-            }
-            acos(n:number):number{
-                return this.answer = Math.acos(n)
-            }
-            sin(n:number):number{
-                return this.answer = Math.sin(n)
-            }
-            tan(n:number):number{
-                return this.answer = Math.tan(n)
-            }
+        return this.answer = n1 - n2
+    }
+    mul(n1:number,n2:number):number{
+        return this.answer = n1 * n2
+    }
+    div(n1:number,n2:number):number{
+        return this.answer = n1 / n2        
+    }    
+    pow(n1:number,n2:number):number{
+        return this.answer = Math.pow(n1,n2)
+    }
+    log10(n:number):number{
+        return this.answer= Math.log10(n)
+    }
+    sqrt(n:number):number{
+        return this.answer = Math.sqrt(n)
+    }
+    cos(n:number):number{
+        return this.answer = Math.cos(n)
+    }
+    acos(n:number):number{
+        return this.answer = Math.acos(n)
+    }
+    sin(n:number):number{
+        return this.answer = Math.sin(n)
+    }
+    tan(n:number):number{
+        return this.answer = Math.tan(n)
+    }
     }
 try {
     const w = new Calculator();
